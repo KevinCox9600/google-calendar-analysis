68b1a544b77d449dbd4793fbb30f982466857612
Kevin Cox <kevindanielcox@gmail.com>
2022-01-13 15:58:06 -0800

    Add global variables file

---
+++ Global Variables.js
@@ -0,0 +1,10 @@
+const COLOR_TO_ACTIVITY_TYPE = {
+    BLUE: "work",
+    BROWN: "default",
+    CYAN: "social",
+    GREEN: "productive",
+    ORANGE: "waste of time",
+    PALE_GREEN: "exercise",
+    PALE_RED: "class",
+    // additional colors: gray, mauve, pale blue, red, yellow
+};
--- code.js
+++ code.js
@@ -1,14 +1,3 @@
-const COLOR_TO_ACTIVITY_TYPE = {
-    BLUE: "work",
-    BROWN: "default",
-    CYAN: "social",
-    GREEN: "productive",
-    ORANGE: "waste of time",
-    PALE_GREEN: "exercise",
-    PALE_RED: "class",
-    // additional colors: gray, mauve, pale blue, red, yellow
-};
-
 function main() {
     const eventsInRange = getEvents();
     const eventsByColor = getEventsByColor(eventsInRange);
