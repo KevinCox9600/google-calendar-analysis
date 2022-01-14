e807621393016c01e082b70d3491940504ae55af
Kevin Cox <kevindanielcox@gmail.com>
2022-01-13 17:06:04 -0800

    Convert tab size to 2

--- Code.js
+++ Code.js
@@ -1,82 +1,88 @@
 function main() {
-    const eventsInRange = getEvents();
-    const eventsByColor = getEventsByColor(eventsInRange);
-    const eventsByActivityType = getEventsByActivityType(eventsByColor);
-    const summary = getSummary(eventsByActivityType);
+  const eventsInRange = getEvents();
+  const eventsByColor = getEventsByColor(eventsInRange);
+  const eventsByActivityType = getEventsByActivityType(eventsByColor);
+  const summary = getSummary(eventsByActivityType);
 
-    console.log(summary);
-    // GmailApp.sendEmail("kevin_cox@brown.edu", "Daily GCal Update", generateSummaryText(summary));
+  console.log(summary);
+  console.log(generateSummaryText(summary));
+  // GmailApp.sendEmail("kevin_cox@brown.edu", "Daily GCal Update", generateSummaryText(summary));
 }
 
 function generateSummaryText(summary) {
-    return ``;
+  let text = `Yesterday, you spent the following amount of time on the following activities:\n`;
+  let bullets = "";
+  for (let category in summary) {
+    bullets += category + '\n';
+  }
+  // `- ${summary.}`;
 }
 
 function getEvents() {
-    calendar = CalendarApp.getDefaultCalendar();
+  calendar = CalendarApp.getDefaultCalendar();
 
-    // get dates
-    let today5am = new Date();
-    today5am.setHours(5, 0, 0, 0);
-    let yesterday5am = new Date(today5am.getTime() - 24 * 60 * 60 * 1000);
+  // get dates
+  let today5am = new Date();
+  today5am.setHours(5, 0, 0, 0);
+  let yesterday5am = new Date(today5am.getTime() - 24 * 60 * 60 * 1000);
 
-    // get events that aren't all day events
-    const events = calendar.getEvents(yesterday5am, today5am);
-    events.filter(event => !event.isAllDayEvent());
-    console.log("num events", events.length);
+  // get events that aren't all day events
+  let events = calendar.getEvents(yesterday5am, today5am);
+  events = events.filter(event => !event.isAllDayEvent());
+  console.log("num events", events.length);
 
-    return events;
+  return events;
 }
 
 function getEventsByColor(events) {
-    // make dict to convert from color to color name
-    const defaultColor = calendar.getColor();
-    const eventColors = CalendarApp.EventColor;
-    delete eventColors.toString;
-    const colorOptions = { ...swap(eventColors) };
-    colorOptions[defaultColor] = "BROWN";
+  // make dict to convert from color to color name
+  const defaultColor = calendar.getColor();
+  const eventColors = CalendarApp.EventColor;
+  delete eventColors.toString;
+  const colorOptions = { ...swap(eventColors) };
+  colorOptions[defaultColor] = "BROWN";
 
-    // make dict of color to events
-    const eventsByColor = {};
-    for (let event of events) {
-        let color = colorOptions[event.getColor() || defaultColor] || "no color found";
-        if (eventsByColor[color] === undefined) {
-            eventsByColor[color] = [event];
-        } else {
-            eventsByColor[color].push(event);
-        }
+  // make dict of color to events
+  const eventsByColor = {};
+  for (let event of events) {
+    let color = colorOptions[event.getColor() || defaultColor] || "no color found";
+    if (eventsByColor[color] === undefined) {
+      eventsByColor[color] = [event];
+    } else {
+      eventsByColor[color].push(event);
     }
-    return eventsByColor;
+  }
+  return eventsByColor;
 }
 
 function swap(json) {
-    var ret = {};
-    for (var key in json) {
-        ret[json[key]] = key;
-    }
-    return ret;
+  var ret = {};
+  for (var key in json) {
+    ret[json[key]] = key;
+  }
+  return ret;
 }
 
 function getEventsByActivityType(eventsByColor) {
-    return Object.keys(eventsByColor).reduce((newObj, color) => {
-        const newKey = COLOR_TO_ACTIVITY_TYPE[color] || "other";
-        newObj[newKey] = eventsByColor[color];
-        return newObj;
-    }, {});
+  return Object.keys(eventsByColor).reduce((newObj, color) => {
+    const newKey = COLOR_TO_ACTIVITY_TYPE[color] || "other";
+    newObj[newKey] = eventsByColor[color];
+    return newObj;
+  }, {});
 }
 
 function getSummary(eventsByActivityType) {
-    summary = {};
-    for (let activityType in eventsByActivityType) {
-        events = eventsByActivityType[activityType];
-        eventDuration = event => (event.getEndTime() - event.getStartTime()) / (3600 * 1000);
-        events.sort((a, b) => eventDuration(b) - eventDuration(a));
-        summary[activityType] = {
-            totalHours: events.reduce((totalHours, event) => totalHours + eventDuration(event), 0),
-            eventNames: events.map(event => event.getSummary()),
-            events,
-        };
-    }
+  summary = {};
+  for (let activityType in eventsByActivityType) {
+    events = eventsByActivityType[activityType];
+    eventDuration = event => (event.getEndTime() - event.getStartTime()) / (3600 * 1000);
+    events.sort((a, b) => eventDuration(b) - eventDuration(a));
+    summary[activityType] = {
+      totalHours: events.reduce((totalHours, event) => totalHours + eventDuration(event), 0),
+      eventNames: events.map(event => event.getSummary()),
+      events,
+    };
+  }
 
-    return summary;
-}
+  return summary;
+};
